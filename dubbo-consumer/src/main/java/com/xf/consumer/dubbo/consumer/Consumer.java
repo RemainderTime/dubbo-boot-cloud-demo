cb807016ffe7ae93badb110355679fc12ba5e553
xiongf <2439534736@qq.com>
2024-11-12 13:11:13 +0800

    初始化dubbo服务订阅者consumer模块,并引入api模块依赖

---
+++ dubbo-consumer/src/main/java/com/xf/consumer/dubbo/consumer/Consumer.java
@@ -0,0 +1,20 @@
+package com.xf.consumer.dubbo.consumer;
+
+import com.xf.api.DemoService;
+import org.apache.dubbo.config.annotation.DubboReference;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer implements CommandLineRunner {
+    @DubboReference
+    private DemoService demoService;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        String result = demoService.sayHello("world");
+        System.out.println("Receive result ======> " + result);
+    }
+}

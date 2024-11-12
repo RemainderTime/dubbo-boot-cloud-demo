b1657467ae9bec603891d83e6ac32c311adaed27
xiongf <2439534736@qq.com>
2024-11-12 12:58:03 +0800

    初始化dubbo公共api模块

---
+++ dubbo-api/src/main/java/com/xf/api/DemoService.java
@@ -0,0 +1,14 @@
+package com.xf.api;
+
+/**
+ * packageName com.xf.api
+ * @author remaindertime
+ * @className DemoService
+ * @date 2024/11/12
+ * @description
+ */
+public interface DemoService {
+
+    String sayHello(String name);
+
+}

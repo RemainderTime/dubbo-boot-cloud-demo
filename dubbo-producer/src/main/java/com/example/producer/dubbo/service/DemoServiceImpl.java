f6a2b5db8f81da889eb3ccf6c882e167dc6885ce
xiongf <2439534736@qq.com>
2024-11-12 13:03:53 +0800

    初始化dubbo服务发布者producer模块,并引入api模块依赖

---
+++ dubbo-producer/src/main/java/com/example/producer/dubbo/service/DemoServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.producer.dubbo.service;
+
+import com.xf.api.DemoService;
+import org.apache.dubbo.config.annotation.DubboService;
+
+@DubboService
+public class DemoServiceImpl implements DemoService {
+
+    @Override
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+}

d79d4d9b1dbdb07efdf88b2168ba9a43647ac9c1
xiongf <2439534736@qq.com>
2024-11-25 17:58:45 +0800

    引入alibaba sentinel 监控接口

--- dubbo-consumer/src/main/java/com/xf/consumer/dubbo/controller/OrderController.java
+++ dubbo-consumer/src/main/java/com/xf/consumer/dubbo/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.xf.consumer.dubbo.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.xf.consumer.dubbo.service.OrderServiceImpl;
+import com.xf.consumer.dubbo.utils.SentinelExceptionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +23,8 @@ public class OrderController {
     private OrderServiceImpl orderService;
 
     @GetMapping("/placeAnOrder")
+    @SentinelResource(value = "placeAnOrder", fallback = "fallbackHandler", fallbackClass = SentinelExceptionUtil.class,
+            blockHandler = "blockExHandler", blockHandlerClass = SentinelExceptionUtil.class)
     public String placeAnOrder() {
         // 模拟用户下单
         orderService.placeAnOrder();
